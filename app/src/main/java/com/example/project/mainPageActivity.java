9157d48d267d9c13890e93772af74f00e1e9633f
tony <tony20191702013@cis.asu.edu.eg>
2022-12-13 17:26:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/project/mainPageActivity.java
@@ -0,0 +1,17 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage_act);
+
+
+
+    }
+}
